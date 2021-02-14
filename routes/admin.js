9a970a48fb9e77731b838be4a2deb48b85d7f6b4
marcosgalvao <marcosgalvaotecnologia@gmail.com>
2021-02-14 11:26:24 -0400

    Cadastrar categoria

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -13,8 +16,23 @@ router.get('/categorias', (req, res) => {
     res.render('admin/categorias')
 })
 
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log('Categoria salva com sucesso!')
+    }).catch((err) => {
+        console.log('Erro ao salvar categoria! ' + err)
+    })
+})
+
 router.get('/categorias/add', (req, res) => {
     res.render('admin/addCategorias')
 })
+
+
 
 module.exports = router
