29635b8981df5e2edb6e978e2cfd816a013db48e
marcosgalvao <marcosgalvaotecnologia@gmail.com>
2021-02-14 13:10:36 -0400

    Validação de formulário de categoria

--- app.js
+++ app.js
@@ -6,11 +6,29 @@ const app = express()
 const admin = require("./routes/admin")
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session');
+const flash = require('connect-flash')
 
 //Configurações
+    //Sessão
+        app.use(session({
+            secret: 'mag000',
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+
+    //Middleware
+        app.use((req,res, next) => {
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next()
+        })
+
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
+
     //Handlebars
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars');
