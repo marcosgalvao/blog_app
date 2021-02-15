69a404be8ce3d0558db35857f75b3ddd101259d3
marcosgalvao <marcosgalvaotecnologia@gmail.com>
2021-02-15 11:13:43 -0400

    Listando postagens

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/Categoria')
 const Categoria = mongoose.model('categorias')
+require('../models/Postagem')
+const Postagem = mongoose.model('postagens')
 
 router.get('/', (req, res) => {
     res.render('admin/index')
@@ -104,7 +106,17 @@ router.post('/categorias/delete', (req, res) => {
 })
 
 router.get('/postagens', (req, res) => {
-    res.render('admin/postagens')
+
+    Postagem.find().lean().populate('categoria').sort({data: 'desc'}).then((postagens) => {
+
+        res.render('admin/postagens', {postagens, postagens})
+
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao listar as postagens.')
+        res.redirect('/admin')
+    })
+
+    
 })
 
 router.get('/postagens/add', (req, res) => {
@@ -115,6 +127,35 @@ router.get('/postagens/add', (req, res) => {
         res.redirect('/admin')
     })
     
+})
+
+router.post('/postagens/nova', (req, res) => {
+
+    var erros = [];
+
+    if (req.body.categoria == "0") {
+        erros.push({texto: 'Categoria inválida, selecione uma categoria!'})
+    }
+    if (erros.length > 0) {
+        res.render('admin/add-postagem', {erros: erros})
+    } else {
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            slug: req.body.slug,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria
+        }
+
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash('success_msg', 'Postagem criada com sucesso!')
+            res.redirect('/admin/postagens')
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro ao salvar a postagem!')
+            res.redirect('/admin/postagens')
+        })
+    }
+
 })
 
 module.exports = router
