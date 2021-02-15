946da73c65b06a416ea962f41946e78918fdc7e0
marcosgalvao <marcosgalvaotecnologia@gmail.com>
2021-02-14 23:05:00 -0400

    Cadastro de postagens

--- routes/admin.js
+++ routes/admin.js
@@ -103,5 +103,18 @@ router.post('/categorias/delete', (req, res) => {
     })
 })
 
+router.get('/postagens', (req, res) => {
+    res.render('admin/postagens')
+})
+
+router.get('/postagens/add', (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render('admin/add-postagem', {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao carregar o formulário')
+        res.redirect('/admin')
+    })
+    
+})
 
 module.exports = router
