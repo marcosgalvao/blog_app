c11436e5233bace021304bfe0ba79d73ec271e1e
marcosgalvao <marcosgalvaotecnologia@gmail.com>
2021-02-15 17:52:30 -0400

    Criando a Home Page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require('path')
 const mongoose = require('mongoose')
 const session = require('express-session');
 const flash = require('connect-flash')
+require('./models/Postagem')
+const Postagem = mongoose.model('postagens')
 
 //Configurações
     //Sessão
@@ -54,7 +56,19 @@ const flash = require('connect-flash')
 
 //Rotas
     app.get('/', (req, res) => {
-        res.send("Rota principal")
+        Postagem.find().lean().populate('categoria').sort({data: 'desc'}).then((postagens) => {
+
+            res.render('index', {postagens, postagens})
+
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro interno.')
+            res.redirect('/404')
+        })
+        
+    })
+
+    app.get('/404', (req, res) => {
+        res.send('Erro 404')
     })
 
     app.get('/posts', (req, res) => {
