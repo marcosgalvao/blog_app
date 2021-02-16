1130fc11bed889bf690d0d0ba06a8fb422163c8f
marcosgalvao <marcosgalvaotecnologia@gmail.com>
2021-02-16 00:02:07 -0400

    Formulário de registro de usuário - com validação

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ require('./models/Postagem')
 const Postagem = mongoose.model('postagens')
 require('./models/Categoria')
 const Categoria = mongoose.model('categorias')
+const usuarios = require('./routes/usuario')
 
 //Configurações
     //Sessão
@@ -117,6 +118,7 @@ const Categoria = mongoose.model('categorias')
     })
 
     app.use('/admin', admin)
+    app.use('/usuarios', usuarios)
 
 //Outros
 const PORT = 8081
