db1181d59b5106196506c170eb518064018bf4f2
marcosgalvao <marcosgalvaotecnologia@gmail.com>
2021-02-18 12:52:09 -0400

    Registre-se - alteração teste

--- config/db.js
+++ config/db.js
@@ -4,6 +4,8 @@ if(process.env.NODE_ENV == 'production'){
 
 }else{
 
-  module.exports = {mongoURI: 'mongodb://localhost/blog_appp'}
+//   module.exports = {mongoURI: 'mongodb://localhost/blog_appp'}
+
+  module.exports = {mongoURI: 'mongodb://marcosgalvaotecnologia:Mag_23degiu@cluster0-shard-00-00.nv1wy.mongodb.net:27017,cluster0-shard-00-01.nv1wy.mongodb.net:27017,cluster0-shard-00-02.nv1wy.mongodb.net:27017/mgtecdb?ssl=true&replicaSet=atlas-ifx9j8-shard-0&authSource=admin&retryWrites=true&w=majority'}
 
 }
