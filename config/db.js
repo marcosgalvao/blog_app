023135c477a2219bf892a03577e782b72e95560d
marcosgalvao <marcosgalvaotecnologia@gmail.com>
2021-02-18 22:58:38 -0400

    Alteração conexão mongo

--- config/db.js
+++ config/db.js
@@ -2,10 +2,16 @@ if(process.env.NODE_ENV == 'production'){
 
     module.exports = {mongoURI: 'mongodb://marcosgalvaotecnologia:Mag_23degiu@cluster0-shard-00-00.nv1wy.mongodb.net:27017,cluster0-shard-00-01.nv1wy.mongodb.net:27017,cluster0-shard-00-02.nv1wy.mongodb.net:27017/mgtecdb?ssl=true&replicaSet=atlas-ifx9j8-shard-0&authSource=admin&retryWrites=true&w=majority'}
 
+    // module.exports = {mongoURI: 'mongodb+srv://marcosgalvaotecnologia:Mag_23degiu@cluster0.nv1wy.mongodb.net/mgtecdb?retryWrites=true&w=majority'}
+   
+   // mongodb+srv://<username>:<password>@cluster0.nv1wy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 }else{
 
 //   module.exports = {mongoURI: 'mongodb://localhost/blog_appp'}
 
   module.exports = {mongoURI: 'mongodb://marcosgalvaotecnologia:Mag_23degiu@cluster0-shard-00-00.nv1wy.mongodb.net:27017,cluster0-shard-00-01.nv1wy.mongodb.net:27017,cluster0-shard-00-02.nv1wy.mongodb.net:27017/mgtecdb?ssl=true&replicaSet=atlas-ifx9j8-shard-0&authSource=admin&retryWrites=true&w=majority'}
+
+  // module.exports = {mongoURI: 'mongodb+srv://marcosgalvaotecnologia:Mag_23degiu@cluster0.nv1wy.mongodb.net/mgtecdb?retryWrites=true&w=majority'}
 
 }
